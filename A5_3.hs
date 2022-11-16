7e9eb57b5f8a80a96930a9cc7fb1db2c5915ac53
FelixHoff1988 <felixjacobhoff@gmail.com>
2022-11-16 22:58:05 +0100

    nonsense...

--- A5_3.hs
+++ A5_3.hs
@@ -1,12 +1,18 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use guards" #-}
 
-data Point = MkPoint Float Float deriving (Eq, Ord, Show)
-data Vector = MkVector Float Float deriving (Eq, Ord, Show)
+data Point = MkPoint {
+    px :: Float,
+    py :: Float
+} deriving (Eq, Ord, Show)
+
+data Vector = MkVector {
+    vx :: Float,
+    vy :: Float
+}deriving (Eq, Ord, Show)
+{-}
 data Triangle = MkTriangle Point Point Point
 data Quad = MkQuad Point Point Point Point
-
-{-
 
 class Polygon p where
     area            :: Polygon p => p -> Float
@@ -16,13 +22,13 @@ class Polygon p where
 
 tt :: Triangle
 tt = MkTriangle x1 x2 x3
-
+{-}
 translate :: Point -> Vector -> Point
 translate (MkPoint p1 p2) (MkVector v1 v2) = MkPoint (p1+v1) (p2+v2)
 
 scale :: Float -> Point -> Point
 scale factor (MkPoint p1 p2) = MkPoint (factor * p1) (factor * p2)
-
+-}
 x1 :: Point
 x1 = MkPoint 2 2
 
@@ -32,6 +38,37 @@ x2 = MkPoint 8 2
 x3 :: Point
 x3 = MkPoint 7 5
 
+v1:: Vector
+v1 = MkVector 2 2
+
+getXfromPoint :: Point -> Float
+getXfromPoint (MkPoint px py) = px
+
+getYfromPoint :: Point -> Float
+getYfromPoint (MkPoint px py) = py
+
+getXfromVector :: Vector -> Float
+getXfromVector (MkVector vx vy) = vx
+
+getYfromVector :: Vector -> Float
+getYfromVector (MkVector vx vy) = vy
+
+translate :: Point -> Vector -> Point
+translate p v = MkPoint (getXfromPoint p + getXfromVector v) (getYfromPoint p + getYfromVector v)
+
+scale :: Point -> Float -> Point
+scale p f = MkPoint (f*getXfromPoint p) (f*getYfromPoint p)
+
+lengthBetweenTwoPoints :: Point -> Point -> Float
+lengthBetweenTwoPoints p1 p2 = sqrt((getXfromPoint p1-getXfromPoint p2)^2 + (getYfromPoint p1- getYfromPoint p2)^2)
+
+data Triangle = MkTriangle {
+    p1 :: Point,
+    p2 :: Point,
+    p3 :: Point
+}
+
+{-
 lengthBetweenTwoPoints :: Point -> Point -> Float
 lengthBetweenTwoPoints (MkPoint x1 y1) (MkPoint x2 y2) = sqrt((x1-x2)^2 + (y1-y2)^2)
 
@@ -52,3 +89,4 @@ areatri (MkPoint x1 y1) (MkPoint x2 y2) (MkPoint x3 y3) = 0.5* findGround (MkPoi
 
 area :: Triangle-> Float
 area (MkTriangle(MkPoint x1 y1) (MkPoint x2 y2) (MkPoint x3 y3)) = 0.5* findGround (MkPoint x1 y1) (MkPoint x2 y2) (MkPoint x3 y3) * calculateHeight (MkPoint x1 y1) (MkPoint x2 y2) (MkPoint x3 y3)
+-}
